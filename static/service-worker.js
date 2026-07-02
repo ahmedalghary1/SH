02cf94e0f0d58c913f95eeff253a9d2620042da5
ahmedalghary1 <ahmedalghary1@gmail.com>
2026-07-02 15:43:56 +0300

    chore: Update cache version to sh-pwa-v2026-07-02-08

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "sh-pwa-v2026-07-02-07";
+const CACHE_VERSION = "sh-pwa-v2026-07-02-08";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 const OFFLINE_URL = "/offline/";
